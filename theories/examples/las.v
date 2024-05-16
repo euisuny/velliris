cf5d9de13581ccf7ed0b6e450fcf16ea9342e0fb
Irene Yoon <euisuny@cis.upenn.edu>
2024-05-16 10:31:21 +0200

    More explanation..

--- theories/examples/las.v
+++ theories/examples/las.v
@@ -11,11 +11,12 @@ Import CFG.
 
 Section las_example.
 
-  (* [a] is the register that will be analyzed for load after store
-     [v] is the current register whose value is stored in the address at [a]. *)
- Definition las_code_body {A : Set} {T}
-   (x: A * instr T) (promotable : raw_id) (stored_val : option raw_id) (c : code T) :=
-    match x with
+  (* [promotable] is the register that will be analyzed for load after store
+     [stored_value] is the current register whose value is stored in the address
+      at [promotable]. *)
+ Definition las_instr {A : Set} {T}
+   (promotable : raw_id) (stored_val : option raw_id) (i : A * instr T) :=
+    match i with
     (* A load instruction from a stored [raw_id] that hasn't been modified. *)
     | (promotable_id, INSTR_Load _ _ (_, EXP_Ident (ID_Local ptr)) _) =>
       if decide (promotable = ptr) then
@@ -24,19 +25,19 @@ Section las_example.
               (* The promotable location has been written to; substitute the load
                  for the local identifier for the promotable location instead. *)
               ((promotable_id, INSTR_Op (EXP_Ident (ID_Local c))), stored_val)
-          | None => (x, stored_val)
+          | None => (i, stored_val)
         end
-      else (x, stored_val)
+      else (i, stored_val)
     (* A store instruction to the promoted local identifier. *)
     | (_, INSTR_Store _
               (_, EXP_Ident (ID_Local v'))
               (_, EXP_Ident (ID_Local ptr)) _) =>
         if decide (promotable = ptr) then
           (* Update the stored value *)
-          (x, Some v')
+          (i, Some v')
         else
-          (x, stored_val)
-    | x => (x, stored_val)
+          (i, stored_val)
+    | x => (i, stored_val)
     end.
 
   (* Given a code block [c] and a promotable location, (i.e. a local identifier
@@ -57,7 +58,7 @@ Section las_example.
     | nil => nil
     | x :: tl =>
         let '(x, v) :=
-          las_code_body x promotable stored_val c
+          las_instr promotable stored_val x
         in
         x :: las_code promotable v tl
     end.
@@ -216,7 +217,7 @@ Proof. Admitted.
 
 Lemma code_WF_las a0 a s:
   code_WF a0 ->
-  code_WF (las_code a0 a s).
+  code_WF (las_code a s a0).
 Proof. Admitted.
 
 (* The [las] algorithm does not change the phi nodes. *)
@@ -229,7 +230,7 @@ Qed.
 (* Well-formedness of ocfg is preserved in the LAS transformation. *)
 Lemma ocfg_WF_las f a:
   ocfg_WF f ->
-  ocfg_WF (las_ocfg f a).
+  ocfg_WF (las_ocfg a f).
 Proof.
   intros. induction f; eauto.
 Admitted.
@@ -250,221 +251,221 @@ Qed.
 
 Section las_example_proof.
 
-Context `{vellirisGS Σ}.
+  Context `{vellirisGS Σ}.
 
-(* TODO *)
-Theorem expr_logrel_relaxed_refl C dt e A_t A_s:
-  (⊢ expr_logrel_relaxed C dt dt e e A_t A_s)%I.
-Proof. Admitted.
+  (* TODO *)
+  Theorem expr_logrel_relaxed_refl C dt e A_t A_s:
+    (⊢ expr_logrel_relaxed C dt dt e e A_t A_s)%I.
+  Proof. Admitted.
 
-Theorem expr_logrel_refl' C dt e A_t A_s l:
-  exp_local_ids e ## l ->
-  (⊢ expr_logrel (local_bij_except l l) alloca_bij
-     C dt dt e e A_t A_s)%I.
-Proof. Admitted.
+  Theorem expr_logrel_refl' C dt e A_t A_s l:
+    exp_local_ids e ## l ->
+    (⊢ expr_logrel (local_bij_except l l) alloca_bij
+      C dt dt e e A_t A_s)%I.
+  Proof. Admitted.
 
-Definition term_local_ids {T} (e : terminator T) : list raw_id.
-Admitted.
+  Definition term_local_ids {T} (e : terminator T) : list raw_id.
+  Admitted.
 
-Theorem term_logrel_refl ϒ C l:
-  term_local_ids ϒ ## l ->
-  (⊢ term_logrel (local_bij_except l l) alloca_bij ϒ ϒ C)%I.
-Proof with vsimp.
-  iIntros (???????) "HI".
-  destruct ϒ eqn: Hϒ; try solve [iDestruct "WF" as "[]"]; cbn.
-  5-8: iApply exp_conv_raise.
-  5 : iApply exp_conv_raiseUB.
-  { (* TERM_Ret *)
-    destruct v. vsimp. Cut.
-    mono: iApply expr_logrel_refl'...
-    { iDestruct "HΦ" as "(Hv & HΦ)"; vfinal. }
-    admit. }
-  { (* TERM_Ret_void *)
-    vfinal. iApply uval_rel_none. }
-  { (* TERM_Br *)
-    destruct v; vsimp...
-    Cut...
-    mono: iApply expr_logrel_refl'...
-    2 : { admit. }
-    Cut... iDestruct "HΦ" as "(Hv & HI)".
-    mono: (iApply (exp_conv_concretize_or_pick with "Hv")) with "[HI]"...
-    destruct dv_s; try iApply exp_conv_raise; [ | iApply exp_conv_raiseUB ].
-    iDestruct (dval_rel_I1_inv with "HΦ") as %->.
-    destruct (DynamicValues.Int1.eq x DynamicValues.Int1.one); vfinal. }
-  { (* TERM_Br_1 *)
-    vfinal. }
-Admitted.
+  Theorem term_logrel_refl ϒ C l:
+    term_local_ids ϒ ## l ->
+    (⊢ term_logrel (local_bij_except l l) alloca_bij ϒ ϒ C)%I.
+  Proof with vsimp.
+    iIntros (???????) "HI".
+    destruct ϒ eqn: Hϒ; try solve [iDestruct "WF" as "[]"]; cbn.
+    5-8: iApply exp_conv_raise.
+    5 : iApply exp_conv_raiseUB.
+    { (* TERM_Ret *)
+      destruct v. vsimp. Cut.
+      mono: iApply expr_logrel_refl'...
+      { iDestruct "HΦ" as "(Hv & HΦ)"; vfinal. }
+      admit. }
+    { (* TERM_Ret_void *)
+      vfinal. iApply uval_rel_none. }
+    { (* TERM_Br *)
+      destruct v; vsimp...
+      Cut...
+      mono: iApply expr_logrel_refl'...
+      2 : { admit. }
+      Cut... iDestruct "HΦ" as "(Hv & HI)".
+      mono: (iApply (exp_conv_concretize_or_pick with "Hv")) with "[HI]"...
+      destruct dv_s; try iApply exp_conv_raise; [ | iApply exp_conv_raiseUB ].
+      iDestruct (dval_rel_I1_inv with "HΦ") as %->.
+      destruct (DynamicValues.Int1.eq x DynamicValues.Int1.one); vfinal. }
+    { (* TERM_Br_1 *)
+      vfinal. }
+  Admitted.
 
-Lemma las_code_body_cases {A T} i c a v id i' v':
-  (id, i', v') = las_code_body (A := A) (T := T) i c a v ->
-  (∃ b t id' e c,
-    i = (id, INSTR_Load b t (id', EXP_Ident (ID_Local a)) e) /\
-    v = Some c /\
-    (id, i') = (id, INSTR_Op (EXP_Ident (ID_Local c))) /\
-    v = v')
+  (* Lemma las_code_body_cases {A T} i c a v id i' v': *)
+  (*   (id, i', v') = las_code_body (A := A) (T := T) i c a v -> *)
+  (*   (∃ b t id' e c, *)
+  (*     i = (id, INSTR_Load b t (id', EXP_Ident (ID_Local a)) e) /\ *)
+  (*     v = Some c /\ *)
+  (*     (id, i') = (id, INSTR_Op (EXP_Ident (ID_Local c))) /\ *)
+  (*     v = v') *)
 
-  \/
+  (*   \/ *)
 
-  (exists b id1 id2 e ve,
-    i = (id, INSTR_Store b
-          (id1, EXP_Ident (ID_Local ve))
-          (id2, EXP_Ident (ID_Local a)) e) /\
-    i = (id, i') /\ v' = Some ve)
+  (*   (exists b id1 id2 e ve, *)
+  (*     i = (id, INSTR_Store b *)
+  (*           (id1, EXP_Ident (ID_Local ve)) *)
+  (*           (id2, EXP_Ident (ID_Local a)) e) /\ *)
+  (*     i = (id, i') /\ v' = Some ve) *)
 
-  \/
+  (*   \/ *)
 
-  (i = (id, i') /\ v = v').
-Proof. Admitted.
+  (*   (i = (id, i') /\ v = v'). *)
+  (* Proof. Admitted. *)
 
-(* TODO Where local_bij_except on the local_ids on the instruction *)
-(* Theorem instr_logrel_refl  *)
+  (* TODO Where local_bij_except on the local_ids on the instruction *)
+  (* Theorem instr_logrel_refl  *)
 
-(* TODO: Promotable block implies points-to *)
-(* Lemma promotable_block_id b id: *)
-(*   promotable_block b id -> *)
+  (* TODO: Promotable block implies points-to *)
+  (* Lemma promotable_block_id b id: *)
+  (*   promotable_block b id -> *)
 
-Lemma las_instr_sim b a v_s i_s ptr:
-  [ a := UVALUE_Addr ptr ]s i_s -∗
-  ptr.1 ↦s v_s  -∗
-  (* State the ownership over [a] *)
-  [∗ list] '(id, i);'(id', i') ∈ las_code b a None; b,
-    ∀ A_t A_s,
-    instr_logrel local_bij alloca_bij id i id' i' ∅ A_t A_s.
-Proof.
-  remember None.
-  iAssert (∀ v, ⌜o = Some v⌝ -∗
-      ∃ ptr', [ v := UVALUE_Addr ptr' ]s i_s ∗
-              ptr'.1 ↦s v_s )%I as "H".
-  { iIntros; subst; inv H0. }
-  clear Heqo.
+  Lemma las_instr_list_sim b a v_s i_s ptr:
+    [ a := UVALUE_Addr ptr ]s i_s -∗
+    ptr.1 ↦s v_s  -∗
+    (* State the ownership over [a] *)
+    [∗ list] '(id, i);'(id', i') ∈ las_code a None b; b,
+      ∀ A_t A_s,
+      instr_logrel local_bij alloca_bij id i id' i' ∅ A_t A_s.
+  Proof.
+    remember None.
+    iAssert (∀ v, ⌜o = Some v⌝ -∗
+        ∃ ptr', [ v := UVALUE_Addr ptr' ]s i_s ∗
+                ptr'.1 ↦s v_s )%I as "H".
+    { iIntros; subst; inv H0. }
+    clear Heqo.
 
-  iInduction b as [ | ] "IH" forall (o) "H"; eauto; cbn.
+    iInduction b as [ | ] "IH" forall (o) "H"; eauto; cbn.
 
-  remember (las_code_body a0 (a0 :: b) a o).
-  destruct p; cbn; destruct p, a0.
-  apply las_code_body_cases in Heqp.
-  destruct Heqp as [ | [ | (?&?)]]; last first.
+    remember (las_code_body a0 (a0 :: b) a o).
+    destruct p; cbn; destruct p, a0.
+    apply las_code_body_cases in Heqp.
+    destruct Heqp as [ | [ | (?&?)]]; last first.
 
-  (* (* Unchanged *) *)
-  (* { inv H0; iSpecialize ("IH" $! o0 with "H"); iFrame "IH". *)
-  (*   admit. } *)
+    (* (* Unchanged *) *)
+    (* { inv H0; iSpecialize ("IH" $! o0 with "H"); iFrame "IH". *)
+    (*   admit. } *)
 
-Admitted.
+  Admitted.
 
-Definition phis_local_ids {T} (e : list (local_id * phi T)) : list raw_id.
-Admitted.
+  Definition phis_local_ids {T} (e : list (local_id * phi T)) : list raw_id.
+  Admitted.
 
-Lemma phis' {T} (Φ : list (local_id * phi T)) l:
-  phis_local_ids Φ ## l ->
-  Φ.*1 ## l /\
-  (forall be ϕ,
-      In ϕ Φ ->
-      let '(Phi _ args) := ϕ.2 in
-      forall e, Util.assoc be args = Some e ->
-      exp_local_ids e ## l).
-Proof. Admitted.
+  Lemma phis' {T} (Φ : list (local_id * phi T)) l:
+    phis_local_ids Φ ## l ->
+    Φ.*1 ## l /\
+    (forall be ϕ,
+        In ϕ Φ ->
+        let '(Phi _ args) := ϕ.2 in
+        forall e, Util.assoc be args = Some e ->
+        exp_local_ids e ## l).
+  Proof. Admitted.
 
-Lemma las_block_sim A_t A_s be b a:
-  block_WF b ->
-  term_local_ids (blk_term b) ## a :: nil ->
-  phis_local_ids (blk_phis b) ## a :: nil ->
-  ⊢ block_logrel (local_bij_except [a] [a]) alloca_bij
-      (las_block a None b) b be ∅ A_t A_s.
-Proof with vsimp.
-  iIntros (WF Ht Hp).
-  apply phis' in Hp; destruct Hp as (Hp1&Hp2).
-  iApply block_compat; eauto.
-  { by apply block_WF_las. }
-  { iApply phis_compat; eauto.
-    cbn. remember (blk_phis b); clear Heql.
-    iInduction l as [ | ] "IH"; eauto.
-    cbn; iSplitL.
-    { destruct a0. iApply phi_compat; destruct p; cbn.
-      destruct (Util.assoc be args) eqn: He; eauto.
-      iApply expr_logrel_refl'.
-      specialize (Hp2 be (l0, Phi t args)); cbn in *.
-      eapply Hp2; eauto. }
+  Lemma las_block_sim A_t A_s be b a:
+    block_WF b ->
+    term_local_ids (blk_term b) ## a :: nil ->
+    phis_local_ids (blk_phis b) ## a :: nil ->
+    ⊢ block_logrel (local_bij_except [a] [a]) alloca_bij
+        (las_block a None b) b be ∅ A_t A_s.
+  Proof with vsimp.
+    iIntros (WF Ht Hp).
+    apply phis' in Hp; destruct Hp as (Hp1&Hp2).
+    iApply block_compat; eauto.
+    { by apply block_WF_las. }
+    { iApply phis_compat; eauto.
+      cbn. remember (blk_phis b); clear Heql.
+      iInduction l as [ | ] "IH"; eauto.
+      cbn; iSplitL.
+      { destruct a0. iApply phi_compat; destruct p; cbn.
+        destruct (Util.assoc be args) eqn: He; eauto.
+        iApply expr_logrel_refl'.
+        specialize (Hp2 be (l0, Phi t args)); cbn in *.
+        eapply Hp2; eauto. }
 
-    iApply "IH"; iPureIntro; eauto.
-    { set_solver. }
-    { intros. destruct ϕ.
-      specialize (Hp2 be0 (l0, p)); destruct p; cbn.
-      intros. eapply Hp2; eauto.
-      by apply in_cons. } }
+      iApply "IH"; iPureIntro; eauto.
+      { set_solver. }
+      { intros. destruct ϕ.
+        specialize (Hp2 be0 (l0, p)); destruct p; cbn.
+        intros. eapply Hp2; eauto.
+        by apply in_cons. } }
 
-  { apply block_WF_inv in WF; destruct WF.
-    iApply code_compat; eauto.
-    { by apply code_WF_las. }
-    cbn. iApply las_instr_sim. }
+    { apply block_WF_inv in WF; destruct WF.
+      iApply code_compat; eauto.
+      { by apply code_WF_las. }
+      cbn. iApply las_instr_sim. }
 
-  { cbn; by iApply term_logrel_refl. }
-Qed.
+    { cbn; by iApply term_logrel_refl. }
+  Qed.
 
-Lemma ocfg_SSA_promotable f a a0:
-  ocfg_is_SSA (a0 :: f) ->
-  promotable_ocfg (a0 :: f) a ->
-  term_local_ids (blk_term a0) ## (a :: nil) /\
-  phis_local_ids (blk_phis a0) ## (a :: nil).
-Proof. Admitted.
+  Lemma ocfg_SSA_promotable f a a0:
+    ocfg_is_SSA (a0 :: f) ->
+    promotable_ocfg (a0 :: f) a ->
+    term_local_ids (blk_term a0) ## (a :: nil) /\
+    phis_local_ids (blk_phis a0) ## (a :: nil).
+  Proof. Admitted.
 
-Lemma las_simulation_ocfg
-  (f : ocfg dtyp) a A_t A_s b1 b2 :
-  ocfg_WF f ->
-  ocfg_is_SSA f ->
-  promotable_ocfg f a ->
-  ⊢ ocfg_logrel
-      (local_bij_except [a] [a])
-      alloca_bij (las_ocfg f a) f ∅ A_t A_s b1 b2.
-Proof.
-  iIntros (???).
-  iApply ocfg_compat; try done.
-  { by eapply ocfg_WF_las. }
-  iModIntro.
-  iInduction f as [ | ] "IH"; eauto.
-  apply ocfg_WF_cons_inv in H0. destruct H0.
-  cbn. iSplitL "".
-  { iSplitL ""; first done.
-    pose proof (ocfg_SSA_promotable _ _ _ H1 H2).
-    destruct H4.
-    iIntros (???); iApply las_block_sim; eauto. }
-  { iApply "IH"; eauto.
-    { iPureIntro; eapply ocfg_is_SSA_cons_inv; eauto. }
-    { iPureIntro. eapply promotable_ocfg_cons_inv; eauto. } }
-Qed.
+  Lemma las_simulation_ocfg
+    (f : ocfg dtyp) a A_t A_s b1 b2 :
+    ocfg_WF f ->
+    ocfg_is_SSA f ->
+    promotable_ocfg f a ->
+    ⊢ ocfg_logrel
+        (local_bij_except [a] [a])
+        alloca_bij (las_ocfg f a) f ∅ A_t A_s b1 b2.
+  Proof.
+    iIntros (???).
+    iApply ocfg_compat; try done.
+    { by eapply ocfg_WF_las. }
+    iModIntro.
+    iInduction f as [ | ] "IH"; eauto.
+    apply ocfg_WF_cons_inv in H0. destruct H0.
+    cbn. iSplitL "".
+    { iSplitL ""; first done.
+      pose proof (ocfg_SSA_promotable _ _ _ H1 H2).
+      destruct H4.
+      iIntros (???); iApply las_block_sim; eauto. }
+    { iApply "IH"; eauto.
+      { iPureIntro; eapply ocfg_is_SSA_cons_inv; eauto. }
+      { iPureIntro. eapply promotable_ocfg_cons_inv; eauto. } }
+  Qed.
 
-Lemma las_simulation_cfg (f : cfg dtyp) a A_t A_s:
-  cfg_WF f ->
-  cfg_is_SSA f ->
-  ⊢ cfg_logrel
-      (local_bij_except [a] [a])
-      alloca_bij (las f) f ∅ A_t A_s.
-Proof.
-  intros. iApply cfg_compat; eauto; last first.
-  { rewrite /las.
-    destruct (find_promotable_alloca f) eqn: Hf; eauto; cbn.
-    { destruct p, i; cbn.
-      {
-Admitted.
+  Lemma las_simulation_cfg (f : cfg dtyp) a A_t A_s:
+    cfg_WF f ->
+    cfg_is_SSA f ->
+    ⊢ cfg_logrel
+        (local_bij_except [a] [a])
+        alloca_bij (las f) f ∅ A_t A_s.
+  Proof.
+    intros. iApply cfg_compat; eauto; last first.
+    { rewrite /las.
+      destruct (find_promotable_alloca f) eqn: Hf; eauto; cbn.
+      { destruct p, i; cbn.
+        {
+  Admitted.
 
-Lemma las_simulation (f : function) :
-  fun_WF (las_fun f) ->
-  fun_WF f ->
-  fun_is_SSA f ->
-  ⊢ fun_logrel (las_fun f) f ∅.
-Proof.
-  iIntros;
-  rewrite /las_fun. destruct f; cbn.
-    iApply fun_compat; last first.
-  { iIntros; cbn.
-    iApply las_simulation_cfg; admit. }
-  all : eauto.
-Admitted.
+  Lemma las_simulation (f : function) :
+    fun_WF (las_fun f) ->
+    fun_WF f ->
+    fun_is_SSA f ->
+    ⊢ fun_logrel (las_fun f) f ∅.
+  Proof.
+    iIntros;
+    rewrite /las_fun. destruct f; cbn.
+      iApply fun_compat; last first.
+    { iIntros; cbn.
+      iApply las_simulation_cfg; admit. }
+    all : eauto.
+  Admitted.
 
-Theorem las_correct (c : function) :
-  fun_is_SSA c ->
-  ctx_ref (las_fun c) c.
-Proof. Admitted.
+  Theorem las_correct (c : function) :
+    fun_is_SSA c ->
+    ctx_ref (las_fun c) c.
+  Proof. Admitted.
 
-(* TODO: Prove it *)
+  (* TODO: Prove it *)
 
 End las_example_proof.
