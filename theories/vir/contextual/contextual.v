e0cb7781fef34c96b9deb8dbf27ea1c4c6dd9d3f
Irene Yoon <euisuny@cis.upenn.edu>
2024-05-22 18:19:22 +0200

    More progress

--- theories/vir/contextual/contextual.v
+++ theories/vir/contextual/contextual.v
@@ -141,20 +141,117 @@ Section contextual_refinement.
       denote_mcfg Ft τ ft args_t ⪯
       denote_mcfg Fs τ fs args_s
     ⦉ fun v_t v_s =>
-          ∃ r_t r_s, ⌜v_t = Ret r_t⌝ ∗ ⌜v_s = Ret r_s⌝ ∗
-          frameR i_t i_s nil nil ∅∗
-          allocaR i_t i_s ∅ ∅ ∗
-          uval_rel r_t r_s ⦊)%I.
+        ∃ r_t r_s, ⌜v_t = Ret r_t⌝ ∗ ⌜v_s = Ret r_s⌝ ∗
+        frameR i_t i_s nil nil ∅∗
+        allocaR i_t i_s ∅ ∅ ∗
+        uval_rel r_t r_s ⦊)%I.
+
+  Definition interp_mrec_body {D E : Type -> Type}
+    (ctx : D ~> itree (D +' E)) : forall T, itree (D +' E) T -> itree E _ :=
+    fun R x =>
+      match observe x with
+      | RetF r => Ret (inr r)
+      | TauF t => Ret (inl t)
+      | @VisF _ _ _ X (inl1 d) k => Ret (inl (x <- ctx _ d;; k x))
+      | @VisF _ _ _ X (inr1 e) k => Vis e (λ x0 : X, Ret (inl (k x0)))
+      end.
+
+  Opaque denote_function.
+  Lemma denote_mcfg_unfold l τ (f : dvalue) args x:
+    lookup_defn f l = Some x ->
+    RelDec.rel_dec (dc_attrs (df_prototype x)) (remove_tag []) ->
+    denote_mcfg l τ f args ≅
+      lr <- interp_mrec_body (denote_mfun l) _ (denote_function x args) ;;
+      match lr with
+      | inl l0 => Tau (interp_mrec (denote_mfun l) l0)
+      | inr r => Ret r
+      end.
+  Proof.
+    intros H Hattr; rewrite /denote_mcfg.
+    rewrite {1}/mrec /interp_mrec.
+    setoid_rewrite unfold_iter at 1.
+    change (ITree.iter _) with (interp_mrec (T := uvalue) (denote_mfun l)).
+    rewrite {1}/denote_mfun. rewrite H.
+    fold (interp_mrec (D := CallE) (E := L0)); rewrite Hattr.
+    eapply eq_itree_clo_bind; first done. intros; subst; done.
+  Qed.
+
+  From Paco Require Import paco.
+
+  Lemma interp_mrec_body_proper {D E} x y ctx ctx':
+    x ≅ y ->
+    (∀ u d, ctx u d ≅ ctx' u d) ->
+    eq_itree (fun x y => match x, y with
+      | inl x, inl y => x ≅ y | inr x, inr y => x = y | _, _ => False end)
+      (interp_mrec_body ctx uvalue x)
+      (interp_mrec_body (D := D) (E := E) ctx' uvalue y).
+  Proof.
+    intros Heq Hctx. punfold Heq. red in Heq.
+    unfold interp_mrec_body.
+    remember (observe x) as Hox; remember (observe y) as Hoy.
+    clear HeqHox HeqHoy.
+    induction Heq; subst; try done; pclearbot; try apply eqit_Ret; try done.
+    destruct e; try apply eqit_Ret.
+    - eapply eq_itree_clo_bind; first done; intros; subst; eauto.
+      eapply REL.
+    - apply eqit_Vis; intros. apply eqit_Ret; apply REL.
+  Qed.
+
+  Lemma interp_mrec_body_proper' {R} (x : itree L0' R) (y : itree L0' R)
+    (ctx ctx' : ∀ T : Type, CallE T → L0'expr T) Φ :
+    (L0'expr_conv x ⪯ L0'expr_conv y
+       ⦉ fun x y => ∃ vx vy, ⌜x = Ret vx⌝ ∗ ⌜y = Ret vy⌝ ∗  Φ vx vy ⦊) -∗
+    (∀ d, L0'expr_conv (ctx R d) ⪯ L0'expr_conv (ctx' R d)
+         ⦉ fun x y => ∃ vx vy, ⌜x = Ret vx⌝ ∗ ⌜y = Ret vy⌝ ∗  Φ vx vy ⦊) -∗
+    interp_mrec_body (D := CallE) (E := L0) ctx R x
+    ⪯
+    interp_mrec_body (D := CallE) ctx' R y
+    ⦉ fun x y =>
+         ∃ vx vy, ⌜x = Ret vx⌝ ∗ ⌜y = Ret vy⌝ ∗
+          match vx, vy with
+          | inl x, inl y => L0'expr_conv x ⪯ L0'expr_conv y [{ (x, y), Φ x y }]
+          | inr x, inr y => Φ x y | _, _ => ⌜False⌝
+          end ⦊.
+  Proof. Admitted.
 
   (** *contextual refinement *)
   Theorem contextual_refinement
     e_t e_s e_t' e_s' C attr attr' ft ft' fs fs' args_t args_s τ:
+    RelDec.rel_dec (dc_attrs (df_prototype e_t)) [] ->
+    RelDec.rel_dec (dc_attrs (df_prototype e_s)) [] ->
     fun_logrel attr_inv attr e_t e_s C -∗
     fun_logrel attr_inv attr' e_t' e_s' C -∗
     mcfg_logrel
       [(ft, e_t); (ft', e_t')]
-      [(fs, e_s); (fs' e_s')] τ ft fs args_t args_s.
+      [(fs, e_s); (fs', e_s')] τ ft fs args_t args_s.
   Proof.
+    intros Hat Has.
+    iIntros "Hf Hf'" (????) "Hfr Ha Hargs".
+    rewrite /mrec /interp_mrec.
+    rewrite denote_mcfg_unfold; cycle 1; cbn.
+    { destruct (RelDec.rel_dec ft ft) eqn: Ht; eauto.
+      apply RelDec.neg_rel_dec_correct in Ht; done. }
+    { done. }
+
+    rewrite denote_mcfg_unfold; cycle 1; cbn.
+    { destruct (RelDec.rel_dec fs fs) eqn: Ht; eauto.
+      apply RelDec.neg_rel_dec_correct in Ht; done. }
+    { done. }
+
+    Cut...
+
+    iPoseProof
+      (interp_mrec_body_proper'
+          (denote_function e_t args_t0)
+          (denote_function e_s args_s0)
+          (denote_mfun [(ft, e_t); (ft', e_t')])
+      ) as "H".
+    iApply sim_expr'_bupd_mono; cycle 1.
+    { iApply ("H" with "[Hf Hfr Ha Hargs]").
+      { iApply "Hf"; admit. }
+      iIntros (?). Require Import Coq.Program.Equality.
+      dependent destruction d; cbn.
+
   Admitted.
 
 End contextual_refinement.
