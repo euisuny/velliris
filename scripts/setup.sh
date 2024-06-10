38f53e5b5de10a94f27ec7c3fb89ca66b84fc66b
Lennard Gäher <l.gaeher@posteo.de>
2024-06-10 20:17:20 +0200

    order

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,7 +13,7 @@ opam pin add coq 8.17.0 $OPAMFLAGS
 opam pin add coq-mathcomp-ssreflect 2.0.0 $OPAMFLAGS
 #make -C vellvm/src opam
 opam install coq-ext-lib coq-paco coq-flocq coq-ceres coq-mathcomp-ssreflect coq-simple-io coq-quickchick ocamlbuild dune menhir qcheck cppo $OPAMFLAGS
-make -C vellvm/src -j
 opam install coq-itree.dev $OPAMFLAGS
 opam install coq-itree-extra $OPAMFLAGS
+make -C vellvm/src -j
 make builddep
