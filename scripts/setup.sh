b3a09ed1fa16d737e0b0fc7eb8d8fb5743d7294a
Lennard Gäher <l.gaeher@posteo.de>
2024-06-10 19:35:46 +0200

    -y

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,12 +8,12 @@ opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo add iris-dev https://gitlab.mpi-sws.org/iris/opam.git
 opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev
 # 8.17.1 does not work because of QuickChick
-opam pin add coq 8.17.0
+opam pin add coq 8.17.0 $OPAMFLAGS
 # this version is required
-opam pin add coq-mathcomp-ssreflect 2.0.0
-make -C vellvm/src opam
+opam pin add coq-mathcomp-ssreflect 2.0.0 $OPAMFLAGS
+#make -C vellvm/src opam
+opam install coq-ext-lib coq-paco coq-flocq coq-ceres coq-mathcomp-ssreflect coq-simple-io coq-quickchick ocamlbuild dune menhir qcheck cppo $OPAMFLAGS
 make -C vellvm/src -j
-#opam install coq-ext-lib coq-paco coq-flocq coq-ceres coq-mathcomp-ssreflect coq-simple-io coq-quickchick ocamlbuild dune menhir qcheck 
-opam install coq-itree.dev
-opam install coq-itree-extra
+opam install coq-itree.dev $OPAMFLAGS
+opam install coq-itree-extra $OPAMFLAGS
 make builddep
